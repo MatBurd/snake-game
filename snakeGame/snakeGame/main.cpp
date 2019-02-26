2048782bafe3be77dd44c3804ec1edc1424059a6
MatBurd <14335572+MatBurd@users.noreply.github.com>
2019-02-25 23:44:56 -0500

    Created the snake-game project in VS

---
+++ snakeGame/snakeGame/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool gameOver;
+
+void SetUp()
+{
+	gameOver = false;
+}
+
+void Draw()
+{
+	
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+
+}
